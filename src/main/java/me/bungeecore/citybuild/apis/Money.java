b7f69706a96587e79c7bf9ca59e92a7bba27fc28
Justin Klein <bungeecore@minecor.de>
2018-06-02 11:06:33 +0200

    no message

--- src/main/java/me/bungeecore/citybuild/apis/Money.java
+++ src/main/java/me/bungeecore/citybuild/apis/Money.java
@@ -1,8 +1,7 @@
 package me.bungeecore.citybuild.apis;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.UUID;
 
 public class Money {
 
@@ -20,7 +19,39 @@ public class Money {
         if (!isConnected()) {
             try {
                 connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Money?autoReconnect=true", "root", "");
-            } catch (SQLException e) { e.printStackTrace(); }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
+
+    private void Update(String qry) {
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(qry);
+            stmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void createTable() {
+        Update("CREATE TABLE IF NOT EXISTS Money (Name VARCHAR(100), UUID VARCHAR(100), Money VARCHAR(100))");
+    }
+
+    private boolean isPlayerExists(UUID uuid) {
+        try {
+            PreparedStatement ps = connection.prepareStatement("SELECT * FROM Money WHERE UUID = '" + uuid.toString() + "'");
+            ResultSet set = ps.executeQuery();
+            boolean get = set.next();
+            set.close();
+            ps.close();
+            return get;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    
 }
